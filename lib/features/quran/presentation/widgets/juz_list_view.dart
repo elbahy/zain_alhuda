2eebd1e9f3b0f322510ee911c6f9f87d62339f09
Mohamed Elbahy <vb.supporter@gmail.com>
2024-04-21 01:15:26 +0200

    juz list view

--- lib/features/quran/presentation/widgets/juz_list_view.dart
+++ lib/features/quran/presentation/widgets/juz_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:zain_alhuda/core/functions/convert_number_to_arabic.dart';
+import 'package:zain_alhuda/core/functions/custome_navigate.dart';
+import 'package:zain_alhuda/features/quran/data/models/juz_model.dart';
 
 class JuzListView extends StatelessWidget {
   const JuzListView({
@@ -7,46 +10,60 @@ class JuzListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const [
-        Text('data1'),
-        Text('data2'),
-        Text('data3'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data4'),
-        Text('data7'),
-      ],
+    return SingleChildScrollView(
+      child: Table(
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+        defaultColumnWidth: const IntrinsicColumnWidth(),
+        border: TableBorder.all(width: 1),
+        // columnWidths: const {
+        //   0: FlexColumnWidth(1),
+        //   1: FlexColumnWidth(4),
+        //   2: FlexColumnWidth(2),
+        //   3: FlexColumnWidth(2),
+        //   4: FlexColumnWidth(4),
+        // },
+        children: [
+          const TableRow(children: [
+            TableCell(
+                child: Text(
+              'م',
+              textAlign: TextAlign.center,
+            )),
+            TableCell(child: Text('اسم الجزء', textAlign: TextAlign.center)),
+            TableCell(child: Text(' الصفحة', textAlign: TextAlign.center)),
+            TableCell(child: Text('السورة', textAlign: TextAlign.center)),
+            TableCell(child: Text('أول كلمة', textAlign: TextAlign.center)),
+          ]),
+          ...List.generate(
+            juzData.length,
+            (index) => TableRow(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    CustomeNavigate.navigate(context, '/quran', extra: juzData[index]['pageNumber']);
+                  },
+                  child: TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Text(juzData[index]['juzNumber'].toString(), textAlign: TextAlign.center),
+                  ),
+                ),
+                GestureDetector(
+                    onTap: () {
+                      CustomeNavigate.navigate(context, '/quran', extra: juzData[index]['pageNumber']);
+                    },
+                    child: TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(convertToArabicRank(juzData[index]['juzNumber'])!, textAlign: TextAlign.center))),
+                GestureDetector(
+                    onTap: () {
+                      CustomeNavigate.navigate(context, '/quran', extra: juzData[index]['pageNumber']);
+                    },
+                    child: TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(juzData[index]['pageNumber'].toString(), textAlign: TextAlign.center))),
+                TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(juzData[index]['surah'].toString(), textAlign: TextAlign.center)),
+                TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(juzData[index]['firstWord'].toString(), textAlign: TextAlign.center)),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
