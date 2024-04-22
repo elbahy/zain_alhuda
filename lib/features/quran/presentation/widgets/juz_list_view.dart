0976fc65c53b1a4cfc3abde479ee1ed1d7001a7c
Mohamed Elbahy <vb.supporter@gmail.com>
2024-04-22 23:30:56 +0200

    juz list view improve design

--- lib/features/quran/presentation/widgets/juz_list_view.dart
+++ lib/features/quran/presentation/widgets/juz_list_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:zain_alhuda/core/functions/convert_number_to_arabic.dart';
 import 'package:zain_alhuda/core/functions/custome_navigate.dart';
+import 'package:zain_alhuda/core/utils/app_colors.dart';
+import 'package:zain_alhuda/core/utils/app_styles.dart';
 import 'package:zain_alhuda/features/quran/data/models/juz_model.dart';
 
 class JuzListView extends StatelessWidget {
@@ -10,60 +12,91 @@ class JuzListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Table(
-        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-        defaultColumnWidth: const IntrinsicColumnWidth(),
-        border: TableBorder.all(width: 1),
-        // columnWidths: const {
-        //   0: FlexColumnWidth(1),
-        //   1: FlexColumnWidth(4),
-        //   2: FlexColumnWidth(2),
-        //   3: FlexColumnWidth(2),
-        //   4: FlexColumnWidth(4),
-        // },
-        children: [
-          const TableRow(children: [
-            TableCell(
-                child: Text(
-              'م',
-              textAlign: TextAlign.center,
-            )),
-            TableCell(child: Text('اسم الجزء', textAlign: TextAlign.center)),
-            TableCell(child: Text(' الصفحة', textAlign: TextAlign.center)),
-            TableCell(child: Text('السورة', textAlign: TextAlign.center)),
-            TableCell(child: Text('أول كلمة', textAlign: TextAlign.center)),
-          ]),
-          ...List.generate(
-            juzData.length,
-            (index) => TableRow(
+    return Column(
+      children: [
+        Table(
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          defaultColumnWidth: const IntrinsicColumnWidth(),
+          border: TableBorder.all(width: 1),
+          columnWidths: const {
+            0: FlexColumnWidth(.75),
+            1: FlexColumnWidth(4.25),
+            2: FlexColumnWidth(1.75),
+            3: FlexColumnWidth(2.2),
+            4: FlexColumnWidth(4.25),
+          },
+          children: [
+            TableRow(children: [
+              const TableCell(
+                  child: Text(
+                'م',
+                textAlign: TextAlign.center,
+              )),
+              const TableCell(
+                  child: Text(
+                'اسم الجزء',
+                textAlign: TextAlign.center,
+                style: AppStyles.elmisri500Size16,
+              )),
+              TableCell(child: Text('الصفحة', textAlign: TextAlign.center, style: AppStyles.elmisri500Size16.copyWith(color: AppColors.thirdColor))),
+              const TableCell(child: Text('السورة', textAlign: TextAlign.center, style: AppStyles.elmisri500Size16)),
+              const TableCell(child: Text('أول كلمة', textAlign: TextAlign.center, style: AppStyles.elmisri500Size16)),
+            ])
+          ],
+        ),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Table(
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              defaultColumnWidth: const IntrinsicColumnWidth(),
+              border: TableBorder.all(width: 1),
+              columnWidths: const {
+                0: FlexColumnWidth(.75),
+                1: FlexColumnWidth(4.5),
+                2: FlexColumnWidth(1.75),
+                3: FlexColumnWidth(2.1),
+                4: FlexColumnWidth(4.5),
+              },
               children: [
-                GestureDetector(
-                  onTap: () {
-                    CustomeNavigate.navigate(context, '/quran', extra: juzData[index]['pageNumber']);
-                  },
-                  child: TableCell(
-                    verticalAlignment: TableCellVerticalAlignment.middle,
-                    child: Text(juzData[index]['juzNumber'].toString(), textAlign: TextAlign.center),
+                ...List.generate(
+                  juzData.length,
+                  (index) => TableRow(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          CustomeNavigate.navigate(context, '/quran', extra: juzData[index]['pageNumber']);
+                        },
+                        child: TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Text(juzData[index]['juzNumber'].toString(), textAlign: TextAlign.center, style: AppStyles.elmisri500Size16),
+                        ),
+                      ),
+                      GestureDetector(
+                          onTap: () {
+                            CustomeNavigate.navigate(context, '/quran', extra: juzData[index]['pageNumber']);
+                          },
+                          child: TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(convertToArabicRank(juzData[index]['juzNumber'])!, textAlign: TextAlign.center, style: AppStyles.elmisri500Size16))),
+                      GestureDetector(
+                          onTap: () {
+                            CustomeNavigate.navigate(context, '/quran', extra: juzData[index]['pageNumber']);
+                          },
+                          child: TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(juzData[index]['pageNumber'].toString(), textAlign: TextAlign.center, style: AppStyles.elmisri500Size16))),
+                      TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(juzData[index]['surah'].toString(), textAlign: TextAlign.center, style: AppStyles.elmisri500Size16)),
+                      TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(juzData[index]['firstWord'].toString(), textAlign: TextAlign.center, style: AppStyles.elmisri500Size16)),
+                    ],
                   ),
                 ),
-                GestureDetector(
-                    onTap: () {
-                      CustomeNavigate.navigate(context, '/quran', extra: juzData[index]['pageNumber']);
-                    },
-                    child: TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(convertToArabicRank(juzData[index]['juzNumber'])!, textAlign: TextAlign.center))),
-                GestureDetector(
-                    onTap: () {
-                      CustomeNavigate.navigate(context, '/quran', extra: juzData[index]['pageNumber']);
-                    },
-                    child: TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(juzData[index]['pageNumber'].toString(), textAlign: TextAlign.center))),
-                TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(juzData[index]['surah'].toString(), textAlign: TextAlign.center)),
-                TableCell(verticalAlignment: TableCellVerticalAlignment.middle, child: Text(juzData[index]['firstWord'].toString(), textAlign: TextAlign.center)),
               ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
